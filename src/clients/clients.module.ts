6926c1f977886ad208b3a41e57e3ba59a9c2bd5d
benmaouche <benmaouche>
2024-06-16 16:19:28 +0200

    feat: add logging system

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { DatabaseModule } from 'src/database/database.module';
-
+import { LoggerMiddleware } from 'src/utils/logger.middleware';
 @Module({
   imports: [DatabaseModule],
   controllers: [ClientsController],
   providers: [ClientsService],
-  
 })
-export class ClientsModule {}
+export class ClientsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,38 +1,56 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import {Prisma,Client} from '@prisma/client'
+import { Injectable, Logger } from '@nestjs/common';
+import { Prisma, Client } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class ClientsService {
-  constructor(private readonly databaseService:DatabaseService){}
+  private readonly logger = new Logger(ClientsService.name);
+
+  constructor(private readonly databaseService: DatabaseService) {}
+
   async create(createClientDto: Prisma.ClientCreateInput) {
+    this.logger.log('Creating a new client', createClientDto);
     return this.databaseService.client.create({ data: createClientDto });
   }
 
-  findAll() {
+  async findAll() {
+    this.logger.log('Fetching all clients');
     return this.databaseService.client.findMany();
   }
 
   async findOne(id: number): Promise<Client | null> {
-    const client = await this.databaseService.client.findUnique({ where: { id } });
+    this.logger.log(`Fetching client with ID ${id}`);
+    const client = await this.databaseService.client.findUnique({
+      where: { id },
+    });
     if (!client) {
-      throw new NotFoundException(`Client with ID ${id} not found`);
+      console.log(`Client with ID ${id} not found`)
+      this.logger.warn(`Client with ID ${id} not found`);
     }
     return client;
   }
 
-  async update(id: number, updateClientDto: Prisma.ClientUpdateInput): Promise<Client> {
+  async update(
+    id: number,
+    updateClientDto: Prisma.ClientUpdateInput,
+  ): Promise<Client> {
+    this.logger.log(`Updating client with ID ${id}`, updateClientDto);
     const existingClient = await this.findOne(id);
-    if (!existingClient){
-      throw new NotFoundException (`Client with ID ${id} not found`);
+    if (!existingClient) {
+      this.logger.warn(`Client with ID ${id} not found`);
     }
-    return this.databaseService.client.update({ where: { id }, data: updateClientDto });
+    return this.databaseService.client.update({
+      where: { id },
+      data: updateClientDto,
+    });
   }
 
   async remove(id: number): Promise<Client> {
+    this.logger.log(`Removing client with ID ${id}`);
     const existingClient = await this.findOne(id);
-    if (!existingClient){
-    throw new NotFoundException (`Client with ID ${id} not found`);}
+    if (!existingClient) {
+      this.logger.warn(`Client with ID ${id} not found`);
+    }
     return this.databaseService.client.delete({ where: { id } });
   }
 }
